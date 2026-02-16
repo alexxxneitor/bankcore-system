e1555aa10f83e11927a6b1b34e36fce8a5f397db
Sebastian Orjuela Giraldo <sebastianorjuelagiraldo@gmail.com>
2026-02-15 19:59:38 -0500

    feat: agregar entidad UserEntity para gestión de clientes

---
+++ ms-customers/src/main/java/com/bankcore/customers/model/UserEntity.java
@@ -0,0 +1,68 @@
+package com.bankcore.customers.model;
+
+import java.time.Instant;
+import java.util.UUID;
+
+import com.bankcore.customers.utils.CustomerStatus;
+import com.bankcore.customers.utils.UserRole;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Builder
+@Table(name = "customers")
+public class UserEntity {
+    
+    @Id @GeneratedValue private UUID id;
+
+    @Column(unique=true, nullable = false)
+    private int dni;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String phone;
+
+    @Column(nullable = false)
+    private String address;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private UserRole role;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private CustomerStatus status;
+
+    @Column(nullable = false, updatable = false, columnDefinition = "TIMESTAMP WITH TIME ZONE")
+    private Instant createdDate;
+
+    @Column(nullable = false, columnDefinition = "TIMESTAMP WITH TIME ZONE")
+    private Instant updatedDate;
+
+    @PrePersist
+    protected void onCreate() {
+        Instant now = Instant.now();
+        this.createdDate = now;
+        this.updatedDate = now;
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        this.updatedDate = Instant.now();
+    }
+}
