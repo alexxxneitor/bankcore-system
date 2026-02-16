2c4a53ce2ea87cac1d0bb2b5c738675b7336fd10
Sebastian Orjuela Giraldo <sebastianorjuelagiraldo@gmail.com>
2026-02-15 21:09:16 -0500

    feat: agregar validación de PIN de cajero automático con AtmPinValidator y anotación ValidAtmPin

---
+++ ms-customers/src/main/java/com/bankcore/customers/dto/request/validators/AtmPinValidator.java
@@ -0,0 +1,17 @@
+package com.bankcore.customers.dto.request.validators;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+public class AtmPinValidator implements ConstraintValidator<ValidAtmPin, String> {
+
+    @Override
+    public boolean isValid(String pin, ConstraintValidatorContext context){
+        if (pin == null) return false;
+        if (!pin.matches("^\\d{4}$")) return false;
+
+        return !pin.equals("0000") && !pin.equals("1234") && !pin.equals("1111") && !pin.equals("2222") &&
+               !pin.equals("3333") && !pin.equals("4444") && !pin.equals("5555") && !pin.equals("6666") &&
+               !pin.equals("7777") && !pin.equals("8888") && !pin.equals("9999");
+    }
+}
---
+++ ms-customers/src/main/java/com/bankcore/customers/dto/request/validators/ValidAtmPin.java
@@ -0,0 +1,21 @@
+package com.bankcore.customers.dto.request.validators;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.FIELD;
+
+@Documented
+@Constraint(validatedBy = AtmPinValidator.class)
+@Target({ FIELD })
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ValidAtmPin {
+    String message() default "Invalid ATM PIN";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
+}
