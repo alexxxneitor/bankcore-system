0546640c06dbe976e0de36d851148ea983fbe75e
Sebastian Orjuela Giraldo <sebastianorjuelagiraldo@gmail.com>
2026-02-16 21:41:31 -0500

    feat: implementar registro de cliente en UserManagementImpl y devolver respuesta adecuada

--- ms-customers/src/main/java/com/bankcore/customers/service/UserManagementImpl.java
+++ ms-customers/src/main/java/com/bankcore/customers/service/UserManagementImpl.java
@@ -3,7 +3,10 @@ package com.bankcore.customers.service;
 import com.bankcore.customers.dto.requests.RegisterRequest;
 import com.bankcore.customers.dto.responses.RegisterResponses;
 import com.bankcore.customers.exception.ResourceConflictException;
+import com.bankcore.customers.model.UserEntity;
 import com.bankcore.customers.repository.UserRepository;
+import com.bankcore.customers.utils.CustomerStatus;
+import com.bankcore.customers.utils.UserRole;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -23,6 +26,29 @@ public class UserManagementImpl implements UserManagement{
         if (userRepository.existsByEmail(request.getEmail())){
             throw new ResourceConflictException("Email already exists");
         }
-        return null;
+
+        UserEntity userEntity =
+                UserEntity.builder()
+                        .dni(request.getDni())
+                        .firstName(request.getFirstName())
+                        .lastName(request.getLastName())
+                        .email(request.getEmail())
+                        .password(request.getPassword())
+                        .ATMPin(request.getAtmPin())
+                        .phone(request.getPhone())
+                        .address(request.getAddress())
+                        .role(UserRole.CUSTOMER)
+                        .status(CustomerStatus.ACTIVE)
+                        .build();
+
+        userRepository.save(userEntity);
+        return RegisterResponses.builder()
+                .id(userEntity.getId())
+                .dni(userEntity.getDni())
+                .fullName(userEntity.getFirstName() + " " + userEntity.getLastName())
+                .email(userEntity.getEmail())
+                .status(userEntity.getStatus())
+                .createdDate(userEntity.getCreatedDate())
+                .build();
     }
 }
