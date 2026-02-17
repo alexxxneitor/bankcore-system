e1bac4dae0e2336ccefab1d1bf4723422dbe319a
Sebastian Orjuela Giraldo <sebastianorjuelagiraldo@gmail.com>
2026-02-16 21:03:19 -0500

    feat: implementar lógica de validación para el registro de clientes en UserManagementImpl

--- ms-customers/src/main/java/com/bankcore/customers/service/UserManagementImpl.java
+++ ms-customers/src/main/java/com/bankcore/customers/service/UserManagementImpl.java
@@ -2,11 +2,26 @@ package com.bankcore.customers.service;
 
 import com.bankcore.customers.dto.requests.RegisterRequest;
 import com.bankcore.customers.dto.responses.RegisterResponses;
+import com.bankcore.customers.repository.UserRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
 
 public class UserManagementImpl implements UserManagement{
 
+    private final UserRepository userRepository;
+
+    public UserManagementImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     @Override
     public RegisterResponses registerCustomer(RegisterRequest request) {
+        if (userRepository.existsByDni(request.getDni())){
+            throw new ResponseStatusException(HttpStatus.CONFLICT, "DNI already exists");
+        }
+        if (userRepository.existsByEmail(request.getEmail())){
+            throw new ResponseStatusException(HttpStatus.CONFLICT, "Email already exists");
+        }
         return null;
     }
 }
