ce2d6603e2250b6c8ac0dd9e94a75a4f60a8646c
Sebastian Orjuela Giraldo <sebastianorjuelagiraldo@gmail.com>
2026-02-16 20:43:25 -0500

    feat: agregar interfaz UserRepository para la gestión de usuarios

---
+++ ms-customers/src/main/java/com/bankcore/customers/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.bankcore.customers.repository;
+
+import com.bankcore.customers.model.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<UserEntity, UUID> {
+
+    boolean existsByDni(int dni);
+
+    boolean existsByEmail(String email);
+}
