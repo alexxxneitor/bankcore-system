c365e661e92eb7e588807c896e1ba208c09e8e07
Sebastian Orjuela Giraldo <sebastianorjuelagiraldo@gmail.com>
2026-02-16 20:36:43 -0500

    feat: implementar clase UserManagementImpl para el registro de clientes

---
+++ ms-customers/src/main/java/com/bankcore/customers/service/UserManagementImpl.java
@@ -0,0 +1,12 @@
+package com.bankcore.customers.service;
+
+import com.bankcore.customers.dto.requests.RegisterRequest;
+import com.bankcore.customers.dto.responses.RegisterResponses;
+
+public class UserManagementImpl implements UserManagement{
+    
+    @Override
+    public RegisterResponses registerCustomer(RegisterRequest request) {
+        return null;
+    }
+}
