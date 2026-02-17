7c43b29f54b246ffb184b3410ab9c16fb016a9bb
Sebastian Orjuela Giraldo <sebastianorjuelagiraldo@gmail.com>
2026-02-16 20:43:16 -0500

    chore: eliminar espacio en blanco innecesario en UserManagementImpl

--- ms-customers/src/main/java/com/bankcore/customers/service/UserManagementImpl.java
+++ ms-customers/src/main/java/com/bankcore/customers/service/UserManagementImpl.java
@@ -4,7 +4,7 @@ import com.bankcore.customers.dto.requests.RegisterRequest;
 import com.bankcore.customers.dto.responses.RegisterResponses;
 
 public class UserManagementImpl implements UserManagement{
-    
+
     @Override
     public RegisterResponses registerCustomer(RegisterRequest request) {
         return null;
