f898811ba79b62be24158a9e7d073808c1a41859
Sebastian Orjuela Giraldo <sebastianorjuelagiraldo@gmail.com>
2026-02-17 18:36:37 -0500

    feat: se crea componente UserMapper para el mapeo automatico de usuaurios registrado

---
+++ ms-customers/src/main/java/com/bankcore/customers/utils/mappers/UserMapper.java
@@ -0,0 +1,19 @@
+package com.bankcore.customers.utils.mappers;
+
+import com.bankcore.customers.dto.responses.RegisterResponses;
+import com.bankcore.customers.model.UserEntity;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
+
+@Mapper(componentModel = "spring")
+public interface UserMapper {
+
+    @Mapping(target = "fullName", source = ".", qualifiedByName = "fullName")
+    RegisterResponses toRegisterResponse(UserEntity user);
+
+    @Named("fullName")
+    default String mapFullName(UserEntity user){
+        return user.getFirstName() + " " + user.getLastName();
+    }
+}
