08a6c362fb6c20a773495026ecfa9090796054f3
Sebastian Orjuela Giraldo <sebastianorjuelagiraldo@gmail.com>
2026-02-17 21:02:36 -0500

    feat: se agregan validacion de null en request, se gregan mas validaciones al atm pin

--- ms-customers/src/main/java/com/bankcore/customers/dto/requests/RegisterRequest.java
+++ ms-customers/src/main/java/com/bankcore/customers/dto/requests/RegisterRequest.java
@@ -3,9 +3,7 @@ package com.bankcore.customers.dto.requests;
 
 import com.bankcore.customers.utils.validators.ValidAtmPin;
 import com.bankcore.customers.utils.validators.ValidPassword;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
@@ -16,30 +14,38 @@ import lombok.Setter;
 public class RegisterRequest {
 
     @NotBlank(message = "DNI is required")
+    @NotNull(message = "DNI cannot be null")
     private String dni;
 
     @NotBlank(message = "First name is required")
+    @NotNull(message = "First name cannot be null")
     private String firstName;
 
     @NotBlank(message = "Last name is required")
+    @NotNull(message = "Last name cannot be null")
     private String lastName;
 
     @NotBlank(message = "Email is required")
+    @NotNull(message = "Email cannot be null")
     @Email(message = "Invalid email format")
     private String email;
 
     @NotBlank(message = "Password is required")
+    @NotNull(message = "Password cannot be null")
     @ValidPassword
     private String password;
 
-    @NotBlank(message = "ATM Pin is required")
+    @NotNull(message = "ATM Pin cannot be null")
     @Size(min = 4, max = 4, message = "ATM Pin must be exactly 4 digits")
+    @Pattern(regexp = "\\d+", message = "ATM Pin must contain only numbers")
     @ValidAtmPin
     private String atmPin;
 
     @NotBlank(message = "Phone number is required")
+    @NotNull(message = "Phone number cannot be null")
     private String phone;
 
     @NotBlank(message = "Address is required")
+    @NotNull(message = "Address cannot be null")
     private String address;
 }
