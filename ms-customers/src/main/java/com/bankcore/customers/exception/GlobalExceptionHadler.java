5cdb3237d4e0c4212472ea73d5f61dcf05ab405d
Sebastian Orjuela Giraldo <sebastianorjuelagiraldo@gmail.com>
2026-02-17 21:03:27 -0500

    feat: se incluye excepcion para los metodos y argumentos no validos

--- ms-customers/src/main/java/com/bankcore/customers/exception/GlobalExceptionHadler.java
+++ ms-customers/src/main/java/com/bankcore/customers/exception/GlobalExceptionHadler.java
@@ -1,8 +1,10 @@
 package com.bankcore.customers.exception;
 
 import com.bankcore.customers.dto.responses.ErrorResponse;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -12,6 +14,21 @@ public class GlobalExceptionHadler {
     @ExceptionHandler(ResourceConflictException.class)
     public ResponseEntity<ErrorResponse> handleResourceConflictException(ResourceConflictException ex) {
         return buildErrorResponse(HttpStatus.CONFLICT, ex.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleValidationException(
+            MethodArgumentNotValidException ex
+    ) {
+
+        String description = ex.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                .findFirst()
+                .orElse("Validation error");
+
+        return buildErrorResponse(HttpStatus.BAD_REQUEST, description);
     }
 
     private ResponseEntity<ErrorResponse> buildErrorResponse(HttpStatus status, String description) {
