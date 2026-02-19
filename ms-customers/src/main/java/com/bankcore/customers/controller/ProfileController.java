0b967e1974c0f9ab7b9215590e3c78f46b2a410f
Cristian Ortiz <corsamps@gmail.com>
2026-02-18 21:17:33 -0500

    feat(ms-customers): implementar controlador de perfil de cliente

---
+++ ms-customers/src/main/java/com/bankcore/customers/controller/ProfileController.java
@@ -0,0 +1,33 @@
+package com.bankcore.customers.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.bankcore.customers.dto.responses.UserProfileResponse;
+import com.bankcore.customers.service.UserManagement;
+
+
+@RestController
+@RequestMapping("/api/customers")
+public class ProfileController {
+    
+    private final UserManagement userManagement;
+    
+    public ProfileController(UserManagement userManagement) {
+        this.userManagement = userManagement;
+    }
+
+
+    @GetMapping("/me")
+    // TODO: reemplazar cuando HU-02 esté lista
+    // @PreAuthorize("isFullyAuthenticated() && hasRole(CUSTOMER)")
+    public ResponseEntity<UserProfileResponse> me(){
+        return ResponseEntity.status(HttpStatus.OK).body(userManagement.getCurrentUserProfile());
+    }
+
+
+}
