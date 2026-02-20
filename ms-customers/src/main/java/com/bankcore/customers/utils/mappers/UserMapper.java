b2e193ee6061cfa4a29089e6bb26648d570c8f7e
Sebastian Orjuela Giraldo <sebastianorjuelagiraldo@gmail.com>
2026-02-19 19:33:40 -0500

    feta: correccion para evitar excepciones `NullPointerException` y se incluyen campos para ignorar en mapeo a respuesta

--- ms-customers/src/main/java/com/bankcore/customers/utils/mappers/UserMapper.java
+++ ms-customers/src/main/java/com/bankcore/customers/utils/mappers/UserMapper.java
@@ -1,19 +1,27 @@
 package com.bankcore.customers.utils.mappers;
 
-import com.bankcore.customers.dto.responses.RegisterResponses;
+import com.bankcore.customers.dto.responses.RegisterResponse;
 import com.bankcore.customers.model.UserEntity;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.Named;
 
+import java.util.Objects;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 @Mapper(componentModel = "spring")
 public interface UserMapper {
 
     @Mapping(target = "fullName", source = ".", qualifiedByName = "fullName")
-    RegisterResponses toRegisterResponse(UserEntity user);
+    @Mapping(target = "password", ignore = true)
+    @Mapping(target = "atmPin", ignore = true)
+    RegisterResponse toRegisterResponse(UserEntity user);
 
     @Named("fullName")
-    default String mapFullName(UserEntity user){
-        return user.getFirstName() + " " + user.getLastName();
+    default String mapFullName(UserEntity user) {
+        return Stream.of(user.getFirstName(), user.getLastName())
+                .filter(Objects::nonNull)
+                .collect(Collectors.joining(" "));
     }
 }
