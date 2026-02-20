12b308af54287fbfa51f3b7ec35102c1aa2d7159
Sebastian Orjuela Giraldo <sebastianorjuelagiraldo@gmail.com>
2026-02-20 17:56:51 -0500

    update claspath de properties

--- ms-customers/src/test/java/com/bankcore/customers/MsCustomersApplicationTests.java
+++ ms-customers/src/test/java/com/bankcore/customers/MsCustomersApplicationTests.java
@@ -13,6 +13,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.List;
@@ -25,6 +26,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @AutoConfigureMockMvc
 @ActiveProfiles("test")
+@TestPropertySource(locations = "classpath:application-test.properties")
 class MsCustomersApplicationTests {
 
     @Test
