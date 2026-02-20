63b27bd53f6122832ab178cc2eea24524cfb0bdd
Cristian Ortiz <corsamps@gmail.com>
2026-02-20 17:19:13 -0500

    refactor(ms-customers): cambiar algunas opciones de seguridad del filterchain

--- ms-customers/src/main/java/com/bankcore/customers/config/SecurityConfig.java
+++ ms-customers/src/main/java/com/bankcore/customers/config/SecurityConfig.java
@@ -1,15 +1,20 @@
 package com.bankcore.customers.config;
 
+import com.bankcore.customers.utils.UserRole;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 @EnableMethodSecurity(securedEnabled = true)
 public class SecurityConfig {
 
@@ -17,12 +22,16 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http
+                .exceptionHandling(e ->
+                        e.authenticationEntryPoint(
+                                (req, res, ex) ->
+                                        res.sendError(HttpServletResponse.SC_UNAUTHORIZED)))
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/auth/**").permitAll()
-                        // TODO: reemplazar cuando HU-02 esté lista 
-                        .requestMatchers("/api/customers/**").permitAll()
-                        .anyRequest().authenticated()
+                        .requestMatchers("/api/customers/me").hasRole(UserRole.CUSTOMER.name())
+                        .anyRequest().denyAll()
                 );
 
         return http.build();
