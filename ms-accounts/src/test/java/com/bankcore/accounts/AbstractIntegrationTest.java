381c30f4bf7d92eff6eb082d52673275e61a8818
Sebastian Orjuela Giraldo <sebastianorjuelagiraldo@gmail.com>
2026-02-21 20:21:16 -0500

    feat: configuracion de test containers para microservicio de accounts

---
+++ ms-accounts/src/test/java/com/bankcore/accounts/AbstractIntegrationTest.java
@@ -0,0 +1,16 @@
+package com.bankcore.accounts;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@Testcontainers
+@SpringBootTest
+@AutoConfigureMockMvc
+public class AbstractIntegrationTest {
+
+    @ServiceConnection
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:17.8");
+}
