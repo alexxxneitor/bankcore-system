80c2bfe7e743c1791986f358b65a55d551d8a77e
Cristian Ortiz <corsamps@gmail.com>
2026-02-24 15:50:00 -0500

    docs(ms-customers): Agregar javadoc a findByEmail del repository

--- ms-customers/src/main/java/com/bankcore/customers/repository/UserRepository.java
+++ ms-customers/src/main/java/com/bankcore/customers/repository/UserRepository.java
@@ -34,6 +34,13 @@ public interface UserRepository extends JpaRepository<UserEntity, UUID> {
      */
     boolean existsByEmail(String email);
 
+    /**
+     * Retrieves a user entity from the database using their email address.
+     *
+     * @param email The email address to search for.
+     * @return An {@link Optional} containing the {@link UserEntity} if found,
+     * or {@link Optional#empty()} if no user matches the provided email.
+     */
     Optional<UserEntity> findByEmail(String email);
 
 }
