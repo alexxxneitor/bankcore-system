22b5b6b015b542a1788a7f0b3ea820e5660a4a6f
Sebastian Orjuela Giraldo <sebastianorjuelagiraldo@gmail.com>
2026-02-25 18:01:01 -0500

    feat(ms-accounts): Creacion de enums para entidad accounts

---
+++ ms-accounts/src/main/java/com/bankcore/accounts/utils/enums/AccountStatus.java
@@ -0,0 +1,8 @@
+package com.bankcore.accounts.utils.enums;
+
+public enum AccountStatus {
+    ACTIVE,
+    INACTIVE,
+    FROZEN,
+    CLOSED
+}
---
+++ ms-accounts/src/main/java/com/bankcore/accounts/utils/enums/AccountType.java
@@ -0,0 +1,6 @@
+package com.bankcore.accounts.utils.enums;
+
+public enum AccountType {
+    SAVINGS,
+    CHECKING
+}
