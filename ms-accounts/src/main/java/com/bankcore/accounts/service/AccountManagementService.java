fb5a720c39807ea25c0627688d527082f7e9141f
Sebastian Orjuela Giraldo <sebastianorjuelagiraldo@gmail.com>
2026-02-27 18:52:17 -0500

    feat: se agrega atributo de id para identificar customer

--- ms-accounts/src/main/java/com/bankcore/accounts/service/AccountManagementService.java
+++ ms-accounts/src/main/java/com/bankcore/accounts/service/AccountManagementService.java
@@ -3,7 +3,9 @@ package com.bankcore.accounts.service;
 import com.bankcore.accounts.dto.requests.AccountRegisterRequest;
 import com.bankcore.accounts.dto.responses.AccountRegisterResponse;
 
+import java.util.UUID;
+
 public interface AccountManagementService {
 
-    AccountRegisterResponse registerAccount(AccountRegisterRequest request);
+    AccountRegisterResponse registerAccount(AccountRegisterRequest request, UUID id);
 }
