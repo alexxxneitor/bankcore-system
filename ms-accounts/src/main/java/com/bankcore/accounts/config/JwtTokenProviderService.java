3531bc262523b89e66840a1318d1084070a73a29
Sebastian Orjuela Giraldo <sebastianorjuelagiraldo@gmail.com>
2026-03-04 22:03:13 -0500

    feat: se agrega codificacion necesaria para sincronizacion entre servicios - se agrega el prefijo "ROLE_" segun configuracion de customers

--- ms-accounts/src/main/java/com/bankcore/accounts/config/JwtTokenProviderService.java
+++ ms-accounts/src/main/java/com/bankcore/accounts/config/JwtTokenProviderService.java
@@ -2,6 +2,8 @@ package com.bankcore.accounts.config;
 
 import com.bankcore.accounts.utils.enums.UserRole;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
+import org.springframework.security.oauth2.jwt.JwsHeader;
 import org.springframework.security.oauth2.jwt.JwtClaimsSet;
 import org.springframework.security.oauth2.jwt.JwtEncoder;
 import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
@@ -53,11 +55,15 @@ public class JwtTokenProviderService {
                 .issuedAt(now)
                 .expiresAt(now.plus(3, ChronoUnit.MINUTES))
                 .subject("BANKCORE_SYSTEM_ACCOUNTS")
-                .claim("roles", List.of(UserRole.SERVICE.name()))
+                .claim("roles", List.of(String.join("", "ROLE_", UserRole.SERVICE.name())))
+                .build();
+
+        JwsHeader jwsHeader = JwsHeader
+                .with(MacAlgorithm.HS256)
                 .build();
 
         return jwtEncoder
-                .encode(JwtEncoderParameters.from(claims))
+                .encode(JwtEncoderParameters.from(jwsHeader, claims))
                 .getTokenValue();
     }
 }
