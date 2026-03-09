76d5dcd1319cb163d5cf249a6d7b4249d218558c
Sebastian Orjuela Giraldo <sebastianorjuelagiraldo@gmail.com>
2026-03-09 18:35:54 -0500

    feat: creacion de enums
    se crean enums para la tabla transacciones

---
+++ ms-accounts/src/main/java/com/bankcore/accounts/utils/enums/TransactionStatus.java
@@ -0,0 +1,9 @@
+package com.bankcore.accounts.utils.enums;
+
+public enum TransactionStatus {
+
+    COMPLETED,
+    PENDING,
+    FAILED,
+    REVERSED
+}
---
+++ ms-accounts/src/main/java/com/bankcore/accounts/utils/enums/TransactionType.java
@@ -0,0 +1,10 @@
+package com.bankcore.accounts.utils.enums;
+
+public enum TransactionType {
+
+    DEPOSIT,
+    WITHDRAWAL,
+    TRANSFER_IN,
+    TRANSFER_OUT,
+    FEE
+}
