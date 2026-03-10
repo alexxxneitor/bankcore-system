1000c157e843679efa2b18976460bbaa4bd66895
Sebastian Orjuela Giraldo <sebastianorjuelagiraldo@gmail.com>
2026-03-10 11:34:13 -0500

    feat: primeras validaciones del servicio
    se crea servicio de transacciones, con validaciones de actividad, a espera de metodo de validacion de pin

--- ms-accounts/src/main/java/com/bankcore/accounts/services/TransactionServiceImpl.java
+++ ms-accounts/src/main/java/com/bankcore/accounts/services/TransactionServiceImpl.java
@@ -2,16 +2,63 @@ package com.bankcore.accounts.services;
 
 import com.bankcore.accounts.dto.requests.TransactionRequest;
 import com.bankcore.accounts.dto.responses.TransactionResponse;
+import com.bankcore.accounts.exceptions.AccountInactiveException;
+import com.bankcore.accounts.exceptions.AccountNotFoundException;
+import com.bankcore.accounts.models.AccountEntity;
+import com.bankcore.accounts.models.TransactionEntity;
+import com.bankcore.accounts.repositories.AccountRepository;
+import com.bankcore.accounts.repositories.TransactionRepository;
+import com.bankcore.accounts.services.complements.CustomerValidationService;
+import com.bankcore.accounts.utils.enums.AccountStatus;
+import com.bankcore.accounts.utils.enums.TransactionStatus;
+import com.bankcore.accounts.utils.enums.TransactionType;
+import com.bankcore.accounts.utils.mappers.TransactionMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.math.BigDecimal;
 import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
 public class TransactionServiceImpl implements TransactionService{
+
+    private final CustomerValidationService validationService;
+    private final AccountRepository accountRepository;
+    private final TransactionRepository transactionRepository;
+    private final TransactionMapper transactionMapper;
+
     @Override
+    @Transactional
     public TransactionResponse makeDeposit(TransactionRequest request, UUID accountId, UUID customerId) {
-        return null;
+
+        validationService.validateCustomerIsActive(customerId);
+
+        AccountEntity account = accountRepository
+                .findByIdAndCustomerId(accountId, customerId)
+                .orElseThrow((AccountNotFoundException::new));
+
+        if (account.getStatus() != AccountStatus.ACTIVE) {
+            throw new AccountInactiveException();
+        }
+
+        BigDecimal newBalance = account.getBalance().add(request.getAmount());
+
+        TransactionEntity transaction = TransactionEntity.builder()
+                .account(account)
+                .type(TransactionType.DEPOSIT)
+                .amount(request.getAmount())
+                .balanceAfter(newBalance)
+                .description(request.getDescription())
+                .status(TransactionStatus.COMPLETED)
+                .build();
+
+        account.setBalance(newBalance);
+
+        transactionRepository.save(transaction);
+        accountRepository.save(account);
+
+        return transactionMapper.toTransactionResponse(transaction);
     }
 }
