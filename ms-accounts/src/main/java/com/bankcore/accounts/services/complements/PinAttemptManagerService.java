03bdeb11713b30b85c863af5a27b557127f60346
Sebastian Orjuela Giraldo <sebastianorjuelagiraldo@gmail.com>
2026-03-15 18:21:57 -0500

    feat: creacion de servicios de intentos
    se crea el servicio de intentos con su respectiva logica de resgistro de intentos y reset de estos, ademas se implementa logica de bloqueo temporal y permanente de cuenta y separacion de excepciones

--- ms-accounts/src/main/java/com/bankcore/accounts/services/complements/PinAttemptManagerService.java
+++ ms-accounts/src/main/java/com/bankcore/accounts/services/complements/PinAttemptManagerService.java
@@ -1,14 +1,114 @@
 package com.bankcore.accounts.services.complements;
 
+import com.bankcore.accounts.exceptions.*;
 import com.bankcore.accounts.integrations.dto.responses.PinValidateResponse;
+import com.bankcore.accounts.models.AccountPinSecurity;
+import com.bankcore.accounts.repositories.AccountPinSecurityRepository;
+import com.bankcore.accounts.utils.enums.AccountStatus;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.time.Duration;
+import java.time.Instant;
 import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
+@Transactional
 public class PinAttemptManagerService {
 
-    public void PinAttemptManager(UUID accountId, PinValidateResponse response){}
+    private static final int TEMP_LOCK_ATTEMPTS = 4;
+    private static final int PERM_LOCK_ATTEMPTS = 8;
+    private static final Duration TEMP_LOCK_DURATION = Duration.ofMinutes(15);
+
+    private final AccountPinSecurityRepository accountPinSecurityRepository;
+
+    public void checkPinLock(UUID accountId){
+        AccountPinSecurity pinSecurity = getAccountPinSecurityEntity(accountId);
+        checkAndThrowIfLocked(pinSecurity);
+    }
+
+    public void processPinAttempt(UUID accountId, PinValidateResponse response) {
+
+        AccountPinSecurity pinSecurity = getAccountPinSecurityEntity(accountId);
+
+        if (response.valid()) {
+            if (!isPinAlreadyClean(pinSecurity)) {
+                resetAttempts(pinSecurity);
+            }
+            return;
+        }
+
+        registerFailedAttempt(pinSecurity);
+
+        checkAndThrowIfLocked(pinSecurity);
+
+        int remaining = calculateRemainingAttempts(pinSecurity);
+        throw  new IncorrectPinException(remaining);
+    }
+
+    @Transactional(readOnly = true)
+    private AccountPinSecurity getAccountPinSecurityEntity(UUID accountId) {
+        return accountPinSecurityRepository
+                .findByAccount_Id(accountId)
+                .orElseThrow(() -> {
+                    log.error("Data inconsistency: AccountPinSecurity not found for accountId={}", accountId);
+                    return new CustomInternalServiceException("Security entity validation error");
+                });
+    }
+
+    private boolean isPinAlreadyClean(AccountPinSecurity pinSecurity) {
+        return pinSecurity.getFailedAttempts() == 0 &&
+                pinSecurity.getTemporaryLockUntil() == null &&
+                !pinSecurity.isPermanentLock();
+    }
+
+    private void resetAttempts(AccountPinSecurity pinSecurity) {
+        pinSecurity.setFailedAttempts(0);
+        pinSecurity.setTemporaryLockUntil(null);
+        pinSecurity.setPermanentLock(false);
+        pinSecurity.setLastFailedAttemptAt(null);
+    }
+
+    private void registerFailedAttempt(AccountPinSecurity pinSecurity) {
+        int attempts = pinSecurity.getFailedAttempts() + 1;
+        pinSecurity.setFailedAttempts(attempts);
+        pinSecurity.setLastFailedAttemptAt(Instant.now());
+
+        if (attempts >= PERM_LOCK_ATTEMPTS) {
+            pinSecurity.getAccount().setStatus(AccountStatus.FROZEN);
+            pinSecurity.setPermanentLock(true);
+            log.warn("Account permanently blocked: accountId={}, status={}",
+                    pinSecurity.getAccount().getId(),
+                    pinSecurity.getAccount().getStatus().name());
+
+        } else if (attempts >= TEMP_LOCK_ATTEMPTS) {
+            pinSecurity.setTemporaryLockUntil(Instant.now().plus(TEMP_LOCK_DURATION));
+            log.warn("Account temporarily locked until {}: accountId={}",
+                    pinSecurity.getTemporaryLockUntil(),
+                    pinSecurity.getAccount().getId());
+        }
+    }
+
+    private int calculateRemainingAttempts(AccountPinSecurity pinSecurity) {
+        if (pinSecurity.getFailedAttempts() < TEMP_LOCK_ATTEMPTS) {
+            return TEMP_LOCK_ATTEMPTS - pinSecurity.getFailedAttempts();
+        } else {
+            return PERM_LOCK_ATTEMPTS - pinSecurity.getFailedAttempts();
+        }
+    }
+
+    private void checkAndThrowIfLocked(AccountPinSecurity pinSecurity) {
+        if (pinSecurity.isPermanentLock()) {
+            throw  new AccountPermanentlyLockedException();
+        }
+
+        if (pinSecurity.getTemporaryLockUntil() != null &&
+                pinSecurity.getTemporaryLockUntil().isAfter(Instant.now())) {
+            throw  new AccountTemporarilyLockedException(pinSecurity.getTemporaryLockUntil());
+        }
+    }
 }
