b12684e5f271cc02af0bdde7144f723b7e2b715e
Sebastian Orjuela Giraldo <sebastianorjuelagiraldo@gmail.com>
2026-03-20 22:05:42 -0500

    feat, creacion de indices para consulta

--- ms-accounts/src/main/java/com/bankcore/accounts/models/TransactionEntity.java
+++ ms-accounts/src/main/java/com/bankcore/accounts/models/TransactionEntity.java
@@ -60,7 +60,9 @@ import java.util.UUID;
         indexes = {
                 @Index(name = "idx_transaction_account", columnList = "account_id"),
                 @Index(name = "idx_transaction_reference", columnList = "referenceNumber"),
-                @Index(name = "idx_transaction_account_created", columnList = "account_id, createdAt DESC")
+                @Index(name = "idx_transaction_account_created", columnList = "account_id, createdAt DESC"),
+                @Index(name = "idx_account_timestamp", columnList = "account_id, timestamp"),
+                @Index(name = "idx_account_type_timestamp", columnList = "account_id, type, timestamp")
         }
 )
 public class TransactionEntity {
