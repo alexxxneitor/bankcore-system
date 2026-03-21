a8a3aeebf3f72cd2224a7443cd21f8ef5e41d118
Sebastian Orjuela Giraldo <sebastianorjuelagiraldo@gmail.com>
2026-03-20 21:28:48 -0500

    feat: dto de respuesta
    se crea clase para dto de respuesta en caso de transacciones basicas

---
+++ ms-accounts/src/main/java/com/bankcore/accounts/dto/responses/TransferHistoryResponse.java
@@ -0,0 +1,48 @@
+package com.bankcore.accounts.dto.responses;
+
+import com.bankcore.accounts.utils.enums.TransactionType;
+import lombok.Builder;
+import lombok.Value;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+import java.util.UUID;
+
+/**
+ * {@code TransferHistoryResponse} is an immutable Data Transfer Object (DTO)
+ * that represents the details of a single transfer transaction in a user's
+ * transaction history.
+ *
+ * <p>This class is typically used as an API response model to provide
+ * clients with structured information about transfers, including
+ * transaction type, amount, balance, and counterparty details.</p>
+ *
+ * <p>Responsibilities:</p>
+ * <ul>
+ *   <li>Expose transaction attributes in a read-only format.</li>
+ *   <li>Provide contextual information such as description and timestamp.</li>
+ *   <li>Ensure immutability through Lombok's {@link lombok.Value} annotation.</li>
+ *   <li>Support builder pattern via {@link lombok.Builder} for easy instantiation.</li>
+ * </ul>
+ *
+ * <p>This object is returned by APIs to provide clients with
+ * transaction history details in a consistent and immutable format.</p>
+ *
+ * @author BankcoreTeam
+ * @author Sebastian Orjuela
+ * @version 1.0
+ * @see TransactionType
+ */
+@Value
+@Builder
+public class TransferHistoryResponse {
+
+    UUID id;
+    TransactionType type;
+    BigDecimal amount;
+    BigDecimal balance;
+    String description;
+    String counterPartyAccount;
+    String counterPartyName;
+    Instant timestamp;
+}
