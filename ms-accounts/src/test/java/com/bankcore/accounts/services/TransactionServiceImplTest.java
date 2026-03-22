929903971284fad8a623db44eaedd0a539613de8
Sebastian Orjuela Giraldo <sebastianorjuelagiraldo@gmail.com>
2026-03-22 14:05:25 -0500

    test: metodo delegador y coordinador

--- ms-accounts/src/test/java/com/bankcore/accounts/services/TransactionServiceImplTest.java
+++ ms-accounts/src/test/java/com/bankcore/accounts/services/TransactionServiceImplTest.java
@@ -1,12 +1,15 @@
 package com.bankcore.accounts.services;
 
+import com.bankcore.accounts.dto.requests.TransactionQueryParams;
 import com.bankcore.accounts.dto.requests.TransactionRequest;
 import com.bankcore.accounts.dto.requests.TransferRequest;
 import com.bankcore.accounts.dto.responses.TransactionResponse;
+import com.bankcore.accounts.dto.responses.TransactionsHistoryResponse;
 import com.bankcore.accounts.dto.responses.TransferResponse;
 import com.bankcore.accounts.models.AccountEntity;
 import com.bankcore.accounts.services.complements.CustomerAccountValidator;
 import com.bankcore.accounts.services.processors.DepositProcessor;
+import com.bankcore.accounts.services.processors.TransactionHistoryProcessor;
 import com.bankcore.accounts.services.processors.TransferProcessor;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -18,6 +21,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.math.BigDecimal;
 import java.util.UUID;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
@@ -33,6 +37,9 @@ public class TransactionServiceImplTest {
 
     @Mock
     private TransferProcessor transferProcessor;
+
+    @Mock
+    private TransactionHistoryProcessor transactionHistoryProcessor;
 
     @InjectMocks
     private TransactionServiceImpl transactionService;
@@ -114,5 +121,19 @@ public class TransactionServiceImplTest {
         );
 
         verify(depositProcessor, never()).processDeposit(any(), any(), any());
+    }
+
+    @Test
+    void shouldDelegateAndReturnResult() {
+        UUID accountId = UUID.randomUUID();
+        TransactionQueryParams filters = new TransactionQueryParams();
+        TransactionsHistoryResponse expected = TransactionsHistoryResponse.builder().build();
+
+        when(transactionHistoryProcessor.getTransactions(accountId, filters)).thenReturn(expected);
+
+        TransactionsHistoryResponse result = transactionService.getTransactionsHistory(accountId, filters);
+
+        assertThat(result).isSameAs(expected);
+        verify(transactionHistoryProcessor).getTransactions(accountId, filters);
     }
 }
